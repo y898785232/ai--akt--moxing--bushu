29dbcee3a1a9335e317e50610b5c7002abdd23fe
y898785232 <104057404+y898785232@users.noreply.github.com>
2026-06-29 21:12:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ cd ~
 git init
 git add index.js
 git commit -m "init"
-git remote add origin https://github.com/你的用户名/ai-server.git
+git remote add origin https://github.com/y898785232/ai--akt--moxing--bushu.git
 git push -u origin main
